3dbe0dd1f19210836de92000e71339d7f61bdb8b
KimGyunYeop <gyop0817@gachon.ac.kr>
2023-11-29 22:06:13 +0900

    Remove ignored files

--- test.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-python -u -m  test.py \
-    --result_path "en_de_dot_reverse_dropout_5000" \
-    --gpu 0
--- visualization.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-python -u -m  visualize_linear.py \
-    --result_path "en_de_base_dot_1eye_softmax_linear7" \
-
-python -u -m  visualize_linear.py \
-    --result_path "en_de_base_dot_2eye_softmax_linear7" \
